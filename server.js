ccbfbb06b6ac39cbba994ecd4a36583ea1c19c65
cdraper07 <39537266+cdraper07@users.noreply.github.com>
2019-06-14 22:27:07 -0700

    Add express/pug packages

---
+++ server.js
@@ -0,0 +1,9 @@
+import express from 'express';
+const app = express()
+app.set('view engine', 'pug')
+app.get('/', (req, res) => {
+    res.render('index')
+})
+app.listen(5656, () => {
+    console.log('http://localhost:5656')
+})
