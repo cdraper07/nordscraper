6adf1ef557b163a5a140e40a39c0b4f9b18a2cab
cdraper07 <39537266+cdraper07@users.noreply.github.com>
2019-06-29 23:09:40 -0700

    Angular implementation and front end cleanup

--- server.js
+++ server.js
@@ -10,10 +10,12 @@ const app = express();
 
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 var db;
+var path = require('path');
+var name;
 
-MongoClient.connect('mongodb+srv://<username>:<password>@nordscraper-62kur.mongodb.net/test', (err, client) => {
+MongoClient.connect('mongodb+srv://collindraper07:skinnyboy@nordscraper-62kur.mongodb.net/test', (err, client) => {
   if (err) return console.log(err)
-  db = client.db('NordScraper') // whatever your database name is
+  db = client.db('NordScraper')
   app.listen(3000, () => {
     console.log('listening on 3000');
   })
@@ -21,7 +23,7 @@ MongoClient.connect('mongodb+srv://<username>:<password>@nordscraper-62kur.mongo
 
 app.use(bodyParser.urlencoded({extended: true}))
 
-cron.schedule("0 0 6 1/1 * ? *", function() {
+cron.schedule("0 8 * * *", function() {
 	db.collection('info').find().toArray(function(err, results) {
   		results.forEach(function(e){ 
   			var obj = JSON.parse(e);
@@ -38,19 +40,27 @@ cron.schedule("0 0 6 1/1 * ? *", function() {
 });
 
 app.get('/', (req, res) => {
- 	res.sendFile('C:/Users/Collin/Documents/GitHub/NordScraper/index.html')
+ 	res.sendFile(path.join(__dirname, 'index.html'));
  	db.collection('info').find().toArray(function(err, results) {
   		console.log(results)
-  		// send HTML file populated with quotes here
 	})
 })
 
+app.get('/info', (req, res) => {
+ 	res.sendFile(path.join(__dirname, 'info.html'));
+})
+
+app.get('/name', function(req, res) {
+  res.send(name);
+});
+
 app.post('/info', (req, res) => {
+  name=req.body.name;
   db.collection('info').save(req.body, (err, result) => {
     if (err) return console.log(err);
 
     console.log('saved to database');
-    res.redirect('/');
+    res.redirect('/info');
   })
 })
 
@@ -59,38 +69,38 @@ function sendEmails(email, url){
 	axios.get(url)
 	.then(response => {
 		
-		nombre = [];
-		linki = [];
-		precio = [];
+		names = [];
+		links = [];
+		prices = [];
 		const $ = cheerio.load(response.data);
 		$('div.product-grid-item__title').each((i, elem) => {
-			nombre.push($(elem).text());
+			names.push($(elem).text());
 		});
 		$('a.product-grid-item.product-grid-item--nordstromrack').each((i, elem) => {
-			linki.push("https://www.nordstromrack.com/"+$(elem).attr('href'));
+			links.push("https://www.nordstromrack.com/"+$(elem).attr('href'));
 		});
 		$('div.product-grid-item__sale span').each((i, elem) => {
-			precio.push($(elem).text());
+			prices.push($(elem).text());
 		});
-		console.log(nombre);
-		console.log(linki);
-		console.log(precio);
+		console.log(names);
+		console.log(links);
+		console.log(prices);
 		
 		const msg = {
   			to: email,
   			from: email,
   			subject: 'Nordstrom Rack Daily Deals',
-  			html: '<a href="'+linki[0]+'">'+nombre[0]+', '+precio[0]+'</a>'+
+  			html: '<a href="'+links[0]+'">'+names[0]+', '+prices[0]+'</a>'+
   				'<br>'+
-  				'<a href="'+linki[1]+'">'+nombre[1]+', '+precio[1]+'</a>'+
+  				'<a href="'+links[1]+'">'+names[1]+', '+prices[1]+'</a>'+
   				'<br>'+
-  				'<a href="'+linki[2]+'">'+nombre[2]+', '+precio[2]+'</a>'+
+  				'<a href="'+links[2]+'">'+names[2]+', '+prices[2]+'</a>'+
   				'<br>'+
-  				'<a href="'+linki[3]+'">'+nombre[3]+', '+precio[3]+'</a>'+
+  				'<a href="'+links[3]+'">'+names[3]+', '+prices[3]+'</a>'+
   				'<br>'+
-  				'<a href="'+linki[4]+'">'+nombre[4]+', '+precio[4]+'</a>'+
+  				'<a href="'+links[4]+'">'+names[4]+', '+prices[4]+'</a>'+
   				'<br>'+
-  				'<a href="'+linki[5]+'">'+nombre[5]+', '+precio[5]+'</a>',
+  				'<a href="'+links[5]+'">'+names[5]+', '+prices[5]+'</a>',
 		};
 		sgMail.send(msg);
 	})
